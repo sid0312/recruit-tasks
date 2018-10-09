af5b1b32d3fb37e723961d4562ffc48d35828877
sid0312 <43300441+sid0312@users.noreply.github.com>
2018-10-09 22:04:24 +0530

    Add files via upload
    This is the draft of my pid program in c++.

---
+++ pid_draft.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class pid
+{
+ public:
+ float Kp,Ki,Kd; //pid constants
+ float error_prev; //previous error
+ float t; //time
+ float max,min;//for pid overshooting
+ float o;
+ pid(float max,float min)
+ {
+   Kp=0.05;
+   Ki=0.000001;
+   Kd=0;
+}
+float calc(float setpoint,float depth)
+{
+ float error=setpoint-depth;
+ float proportional=Kp*error;
+ float i;
+ i=i+error*t;
+ float integral=Ki*i;
+ float d=(error-error_prev)/t;
+ float derivative=Kd*d;
+ float o=proportional+integral+derivative;
+//regulation of overshoots
+ if(o>max)
+ o=max;
+ else if(o<min)
+ o=min;
+error_prev=error;
+
+return o;
+}
+~pid()
+{}
+};
+
+   
+ 
+
+ 
+
